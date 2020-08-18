8a1d35c32e729a82280b576235021cc1039e4336
Tesfaye Gari <TGari@chemtrec.com>
2020-08-18 19:51:44 -0400

    jQuery and JS examples

--- FirstHtml/Programming/JSDemo1/mycustom.js
+++ FirstHtml/Programming/JSDemo1/mycustom.js
@@ -1,1 +1,33 @@
-alert("This is amazing")
+function customCalculator() {
+  var num1 = document.getElementById('number1').value * 1; //$('#number1').val()*1;
+  var num2 = document.getElementById('number2').value * 1;//$('#number1').val()*1;
+  var result = num1 + num2;
+  //document.getElementById('result').innerHTML = 'Sum of the two numbers is ' + result;
+  $('#result').html('Sum of the two numbers is ' + result);
+}
+
+function getListItems(listName){
+  var listItemsUrl =''
+  return $.ajax({
+    url: listItemsUrl,
+    method: "POST",
+    headers: { "Accept": "application/json; odata=nometadata" },
+    data: jsonData
+  });
+}
+
+$(document).ready(function () {
+  //code goes here 
+  $('#hideText').click(function () {
+    $('#hideme').hide();
+  });
+  $('#showText').click(function () {
+    $('#hideme').show();
+  });
+  $('#submit').click(function () {
+    customCalculator();
+  });
+
+  
+
+});
